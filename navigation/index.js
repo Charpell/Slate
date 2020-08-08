46951aeb2fb5a5fad0750015f309ff1fe08d4fe4
Ebuka Umeh <ebukaumeh@Ebukas-MacBook-Pro.local>
2020-08-08 19:34:37 +0100

    search

--- navigation/index.js
+++ navigation/index.js
@@ -2,11 +2,22 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import StackHome from './StackHome'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={StackHome} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-        <StackHome />
+        <MyTabs />
     </NavigationContainer>
   );
 }
