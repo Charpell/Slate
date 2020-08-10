1210df8b4dbfaf3935458feb9b7d5dee5370ef07
Ebuka Umeh <ebukaumeh@Ebukas-MacBook-Pro.local>
2020-08-10 20:19:05 +0100

    games

---
+++ gameData.js
@@ -0,0 +1,74 @@
+export default games = [
+    {
+        id: 1,
+        title: "Unforseen Incidents",
+        teaser: "A classical style interactive mystery set in a beautifully hand-painted world",
+        description: "A classical style interactive mystery set in a beautifully hand-painted world A classical style interactive mystery set in a beautifully hand-painted world",
+        rating: 4.5,
+        category: ["Action", "Indie"],
+        age: "12+",
+        cover: require('./assets/un.jpg'),
+        screenshots: [
+            require('./assets/pn.jpeg'),
+            require('./assets/pn.jpeg'),
+            require('./assets/pn.jpeg'),
+            require('./assets/pn.jpeg'),
+            require('./assets/pn.jpeg')
+        ],
+        backgroundColor: "#824571"
+    },
+    {
+        id: 2,
+        title: "Unforseen Incidents",
+        teaser: "A classical style interactive mystery set in a beautifully hand-painted world",
+        description: "A classical style interactive mystery set in a beautifully hand-painted world A classical style interactive mystery set in a beautifully hand-painted world",
+        rating: 4.5,
+        category: ["Action", "Indie"],
+        age: "12+",
+        cover: require('./assets/un.jpg'),
+        screenshots: [
+            require('./assets/pn.jpeg'),
+            require('./assets/pn.jpeg'),
+            require('./assets/pn.jpeg'),
+            require('./assets/pn.jpeg'),
+            require('./assets/pn.jpeg')
+        ],
+        backgroundColor: "#824571"
+    },
+    {
+        id: 3,
+        title: "Unforseen Incidents",
+        teaser: "A classical style interactive mystery set in a beautifully hand-painted world",
+        description: "A classical style interactive mystery set in a beautifully hand-painted world A classical style interactive mystery set in a beautifully hand-painted world",
+        rating: 4.5,
+        category: ["Action", "Indie"],
+        age: "12+",
+        cover: require('./assets/un.jpg'),
+        screenshots: [
+            require('./assets/pn.jpeg'),
+            require('./assets/pn.jpeg'),
+            require('./assets/pn.jpeg'),
+            require('./assets/pn.jpeg'),
+            require('./assets/pn.jpeg')
+        ],
+        backgroundColor: "#824571"
+    },
+    {
+        id: 4,
+        title: "Unforseen Incidents",
+        teaser: "A classical style interactive mystery set in a beautifully hand-painted world",
+        description: "A classical style interactive mystery set in a beautifully hand-painted world A classical style interactive mystery set in a beautifully hand-painted world",
+        rating: 4.5,
+        category: ["Action", "Indie"],
+        age: "12+",
+        cover: require('./assets/un.jpg'),
+        screenshots: [
+            require('./assets/pn.jpeg'),
+            require('./assets/pn.jpeg'),
+            require('./assets/pn.jpeg'),
+            require('./assets/pn.jpeg'),
+            require('./assets/pn.jpeg')
+        ],
+        backgroundColor: "#824571"
+    }
+]
